9d6fbf864d5651ca6ae8200e0b86a04b9303e7c4
ft-noone <1430621980@qqcom>
2021-01-18 21:36:51 +0800

    红黑树

--- 红黑树/红黑树.js
+++ 红黑树/红黑树.js
@@ -1,21 +1,30 @@
-class bst{//二叉搜索树，左小右大binary search tree
-    tree = this;
-    root = null;
+var color = {red: 1, black: 0};//1: red, 0: black;
+//红黑树性质：
+// 1.每个结点或是红色的，或是黑色的
+// 2.根结点是黑色的
+// 3.每个叶结点是黑色的：指所有的null指向一个黑色的节点（哨兵）
+// 4.如果一个结点是红色的，则它的两个子结点都是黑色的
+// 5.对每个结点，从该结点到其所有后代叶结点的简单路径上，均包含相同数目的黑色结点
+
+class redBlackT{
     eleNum = 0;
-    constructor(tree){
-        if(tree instanceof bst) {
-            this.tree = tree;
-            this.root = tree.root;
-            this.eleNum = tree.eleNum;
-        }
+    blackHeight = 0;
+    constructor(){
+        this.nullNode = new treenode();
+        this.nullNode.color = color.black;
+        this.root = null;
     }
     getRoot(){
         return this.root;
     }
     insert(key, ele, node){
-        if(!(node instanceof treenode)) node = this.root;
+        if(!(node instanceof treenode)) node = this.root;//如果是null或者其它，则置node为根节点
         var temp = new treenode(key, ele);
-        if(node == null) this.root = temp;
+        if(node == null) {//只有赋值为根节点时才有可能为null，红黑树性质，根节点必须为黑色
+            temp.color = color.black;
+            this.root = temp;
+            this.blackHeight ++;
+        }
         else{
             var pre;
             while(node != null){
@@ -26,22 +35,91 @@ class bst{//二叉搜索树，左小右大binary search tree
             if(key > pre.key) pre.right = temp;
             else pre.left = temp;
             temp.p = pre;
+            this.fixup(temp);
         }
         this.eleNum ++;
+        return true;
     }
-    recursionInsert(key, ele, node){//递归插入
-        if(!(node instanceof treenode)) node = this.root;
-        var temp = new treenode(key, ele);
-        if(node == null) this.root = temp;
-        else this.subRecursion(node, temp);
+    fixup(node){
+        if(node.color != color.red) {
+            console.log("the node fixed must be red");
+            return ;
+        }
+        var temp;
+        while(node.p.color == color.red){
+            temp = node.p.p;//因为进入循环时满足，node父节点为红，因为红黑树性质2，所以必然表示根节点，不是根节点则存在父节点，即node.p.p存在
+            if(node.p == temp.left){//node.p是node.p.p的left/right时的情况对称，交换left和right即可
+                if(temp.right != null && temp.right.color == color.red){//node.p.p等于null时表示为黑色，情况1
+                    temp.color = color.red;//这种情况改变node.p.p颜色为红时，仍然保持红黑树性质5，因为左右子节点原本是红色，都变成黑色，则路径上的黑色节点数不变
+                    temp.right.color = color.black;
+                    node.p.color = color.black;
+                    node = temp;
+                    if(node.p == null) {//处理根节点的情况
+                        node.color = color.black;
+                        this.blackHeight ++;
+                        break;
+                    }
+                    continue;
+                }else if(node == node.p.right){//把情况2转换为情况3
+                    node = node.p;
+                    this.leftRotate(node);
+                }
+                node.p.color = color.black;//处理情况3
+                temp.color = color.red;
+                this.rightRotate(temp);
+                break;
+            }else{
+                if(temp.left != null && temp.left.color == color.red){
+                    temp.color = color.red;
+                    temp.left.color = color.black;
+                    node.p.color = color.black;
+                    node = temp;
+                    if(node.p == null) {
+                        node.color = color.black;
+                        this.blackHeight ++;
+                        break;
+                    }
+                    continue;
+                }else if(node == node.p.left){
+                    node = node.p;
+                    this.rightRotate(node);
+                }
+                node.p.color = color.black;
+                temp.color = color.red;
+                this.leftRotate(temp);
+                break;
+            }
+        }
     }
-    subRecursion(a, b){
-        if(a.key > b.key){
-            if(a.left == null) a.left = b;
-            else this.subRecursion(a.left, b);
-        }else if(a.right == null){
-            a.right = b;
-        }else this.subRecursion(a.right, b);
+    leftRotate(node){
+        if(node.right == null){
+            console.log("the node must have a right child");
+            return ;
+        }
+        var y = node.right;
+        node.right = y.left;
+        if(y.left != null) y.left.p = node;
+        y.left = node;
+        if(node.p == null) this.root = y;
+        else if(node.p.left == node) node.p.left = y;
+        else node.p.right = y;
+        y.p = node.p;
+        node.p = y;
+    }
+    rightRotate(node){
+        if(node.left == null){
+            console.log("the node must have a left child");
+            return ;
+        }
+        var y = node.left;
+        node.left = y.right;
+        if(y.right != null) y.right.p = node;
+        y.right = node;
+        if(node.p == null) this.root = y;
+        else if(node.p.left == node) node.p.left = y;
+        else node.p.right = y;
+        y.p = node.p;
+        node.p = y;
     }
     delete(node){//删除一个元素节点，便从左子树取得最大的元素，放在节点处，并删除左子树的最大元素
         if(!(node instanceof treenode)) return;
@@ -123,9 +201,23 @@ class bst{//二叉搜索树，左小右大binary search tree
         console.log(str + "size: " + this.eleNum);
     }
     check(){
-        var node = this.root, prev = null, i = 0;
+        var node = this.root, prev = null, i = 0, blackcount = 0;
+        if(node != null && node.color != color.black) {
+            console.log("root node must be black");
+            return false;
+        }
         while(node != null){
-            if(node.p != prev) return false;
+            if(node.p != prev) {
+                console.log("parent of node is worry, parent: ", prev, "child: ", node);
+                return false;
+            }
+            if(prev != null && prev.color == color.red){
+                if(node.color != color.black) {
+                    console.log("parent is red, child is red, parent: ", prev, "child: ", node);
+                    return false;
+                }
+            }
+            if(node.color == color.black) blackcount++;
             i ++;
             prev = node;
             if(node.left != null){
@@ -133,12 +225,20 @@ class bst{//二叉搜索树，左小右大binary search tree
             }else if(node.right != null){
                 node = node.right;
             }else{
+                if(blackcount != this.blackHeight) {
+                    console.log("black height is worry: ", blackcount, "fact: ", this.blackHeight);
+                    return false;
+                }
                 let j = 0;
                 while(node.p != null){
                     prev = node;
                     node = node.p;
+                    if(prev.color == color.black) blackcount --;
                     j ++;
-                    if(j > this.eleNum) return false;
+                    if(j > this.eleNum) {
+                        console.log("in up recursion: ", j, "all node num: ", this.eleNum);
+                        return false;
+                    }
                     if(node.left != null && prev == node.left && node.right != null){
                         prev = node;
                         node = node.right;
@@ -147,13 +247,21 @@ class bst{//二叉搜索树，左小右大binary search tree
                 }
                 if(node.p == null) break;
             }
-            if(i > this.eleNum) return false;
+            if(i > this.eleNum) {
+                console.log("in down recursion: ", i, "all node num: ", this.eleNum);
+                return false;
+            }
         }
-        if(i < this.eleNum) return false;
+        if(i < this.eleNum) {
+            console.log("check node num: ", i, "all node num: ", this.eleNum);
+            return false;
+        }
         return true;
     }
 }
-class bstnode{
+
+class treenode{
+    color = color.red;//1: red, 0: black;
     p = null;
     left = null;
     right = null;
