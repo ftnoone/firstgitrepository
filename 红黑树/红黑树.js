682e344ba88a30fbf347258f02b640fb2271f34e
ft-noone <1430621980@qqcom>
2021-01-23 17:44:40 +0800

    红黑树ok

--- 红黑树/红黑树.js
+++ 红黑树/红黑树.js
@@ -35,12 +35,11 @@ class redBlackT{
             if(key > pre.key) pre.right = temp;
             else pre.left = temp;
             temp.p = pre;
-            this.fixup(temp);
+            this.insertFixup(temp);
         }
         this.eleNum ++;
-        return true;
     }
-    fixup(node){
+    insertFixup(node){
         if(node.color != color.red) {
             console.log("the node fixed must be red");
             return ;
@@ -121,21 +120,103 @@ class redBlackT{
         y.p = node.p;
         node.p = y;
     }
+    deleteFixup(node, p){
+        var w;
+        while(this.isBlack(node)){//node代表黑高变化的红黑子树的根节点，即从node节点开始，所有node的子节点黑高都少一
+            if(node == this.root){//只有删除到根节点，当从根节点开始的所有子节点的黑高都少一，红黑树的整体黑高才减一，退出循环即可
+                this.blackHeight --;
+                break;
+            }
+            if(node == p.left){
+                w = p.right;
+                if(w.color == color.red){//情况一：父节点的右子节点是红色，则父节点必然是黑色，转换为情况二
+                    p.color = color.red;
+                    w.color = color.black;
+                    this.leftRotate(p);
+                    w = p.right;
+                }
+                if(this.isBlack(w.left) && this.isBlack(w.right)){//情况二：当父节点的右子节点的所有子节点都是黑色，将父节点的右子树的根节点变为红色，使其黑高减一，将node指向父节点，以父节点p为根节点的子树整体黑高减一
+                    w.color = color.red;
+                    node = p;
+                    p = node.p;
+                }else{
+                    if(this.isBlack(w.right)){//情况三，w节点的右节点是黑色，将其转换为情况四
+                        w.color = color.red;
+                        w.left.color = color.black;
+                        this.rightRotate(w);
+                        w = p.right;
+                    }
+                    w.right.color = color.black;//情况四，w，有右红子节点，将w黑节点（w如果为红，情况一会处理）移到node子树，并将右子红节点变为黑色，整体黑高变化变化回来
+                    w.color = p.color;
+                    p.color = color.black;
+                    this.leftRotate(p);
+                    node = this.root;
+                    break;
+                }
+            }else{//同上，左右互换即可
+                w = p.left;
+                if(w.color == color.red){
+                    p.color = color.red;
+                    w.color = color.black;
+                    this.rightRotate(p);
+                    w = p.left;
+                }
+                if(this.isBlack(w.left) && this.isBlack(w.right)){
+                    w.color = color.red;
+                    node = p;
+                    p = node.p;
+                }else{
+                    if(this.isBlack(w.left)){
+                        w.color = color.red;
+                        w.right.color = color.black;
+                        this.leftRotate(w);
+                        w = p.left;
+                    }
+                    w.left.color = color.black;
+                    w.color = p.color;
+                    p.color = color.black;
+                    this.rightRotate(p);
+                    node = this.root;
+                    break;
+                }
+            }
+        }
+        if(node != null) node.color = color.black;
+    }
+    isBlack(node){
+        return node == null || node.color == color.black;
+    }
     delete(node){//删除一个元素节点，便从左子树取得最大的元素，放在节点处，并删除左子树的最大元素
         if(!(node instanceof treenode)) return;
-        if(node.left == null) this.transplant(node, node.right);
-        else if(node.right == null) this.transplant(node, node.left);
-        else {
+        var originColor = node.color, x, p;
+        if(node.left == null) {
+            x = node.right;
+            p = node.p;
+            this.transplant(node, node.right);
+        }
+        else if(node.right == null) {
+            x = node.left;
+            p = node.p;
+            this.transplant(node, node.left);
+        }
+        else {//这里删除的是leftmax，node节点的元素和键值变化为leftmax的元素和键值
             var leftMax = this.maximum(node.left);
+            originColor = leftMax.color;
+            x = leftMax.left;
+            p = leftMax.p;
             this.transplant(leftMax, leftMax.left);//二叉树性质，因为最大，所以这个元素必然没有右子树，把它的左子树移到本身即可
             node.key = leftMax.key;
             node.ele = leftMax.ele;
+
             //if(leftMax == node.left) node.left = null;
             // if(leftMax.p != node) {
             //     this.transplant(leftMax, leftMax.left);
             // }
         }
         this.eleNum --;
+        if(originColor == color.black) {//只有删除的节点是黑色时，才会有黑高的变化，才要修复红黑树
+            this.deleteFixup(x, p);
+        }
     }
     transplant(a, b){//把b移到a处，移植二叉树
         var p = a.p;
@@ -179,16 +260,14 @@ class redBlackT{
         return node;
     }
     maximum(node){
-        if(!(node instanceof treenode)) node = this.root;
-        if(node == null) return null;
+        if(!(node instanceof treenode)) return null;
         while(node.right != null){
             node = node.right;
         }
         return node;
     }
     minimum(node){
-        if(!(node instanceof treenode)) node = this.root;
-        if(node == null) return null;
+        if(!(node instanceof treenode)) return null;
         while(node.left != null) node = node.left;
         return node;
     }
@@ -218,6 +297,14 @@ class redBlackT{
                 }
             }
             if(node.color == color.black) blackcount++;
+            else {
+                if(node.left){
+                    if(node.left.color != color.black || node.right.color != color.black) {
+                        console.log("red node child are not black: ", node);
+                        return false;
+                    }
+                }
+            }
             i ++;
             prev = node;
             if(node.left != null){
@@ -280,8 +367,8 @@ class treenode{
 var random = Math.random, floor = Math.floor, bba, arr2, arr3;
 function test(){
     var arr = new Array(300);
-    arr2 = new Array(30);
-    bba = new bst();
+    arr2 = new Array(100);
+    bba = new redBlackT();
     for(let i = 0; i < 300; i ++){
         arr[i] = floor(random() * 10000);
         bba.insert(arr[i]);
@@ -291,18 +378,21 @@ function test(){
     for(let i = 0; i < 300; i ++){
         arr3[i] = arr[i];
     }
-    var node = bba.minimum();
+    var node = bba.minimum(bba.root);
     for(let i = 0; i < 300; i ++){
         if(node.key != arr[i]) return false;
         node = bba.successor(node);
     }
-    node = bba.maximum();
+    node = bba.maximum(bba.root);
     for(let i = 299; i >= 0; i --){
         if(node.key != arr[i]) return false;
         node = bba.predecessor(node);
     }
-    console.log("insert yes");
-    for(let i = 0, num, limit = 300, box, temp; i < 30; i ++){
+    if(!bba.check()){
+        console.log("insert error");
+        return false;
+    }
+    for(let i = 0, num, limit = 300, box, temp; i < 100; i ++){
         num = floor(random() * limit);
         node = bba.search(arr[num]);
         arr2[i] = arr[num];
@@ -321,23 +411,31 @@ function test(){
         limit --;
     }
     arr.sort((a, b)=>a - b);
-    node = bba.minimum();
+    node = bba.minimum(bba.root);
     for(let i = 0; i < arr.length; i ++){
         if(node.key != arr[i]){
             console.log("false",node.key, arr[i], i);
-            for(let i = 0; i < 30; i ++) if(arr2[i] == node.key) consolr.log("had deleted", arr2[i])
+            for(let i = 0; i < 100; i ++) if(arr2[i] == node.key) consolr.log("had deleted", arr2[i])
             return false;
         }
         node = bba.successor(node);
     }
-    node = bba.maximum();
+    node = bba.maximum(bba.root);
     for(let i = arr.length - 1; i >= 0; i --){
         if(node.key != arr[i]){
             console.log("false",node.key, arr[i], i);
-            for(let i = 0; i < 30; i ++) if(arr2[i] == node.key) consolr.log("had deleted", arr2[i])
+            for(let i = 0; i < 100; i ++) if(arr2[i] == node.key) consolr.log("had deleted", arr2[i])
             return false;
         }
         node = bba.predecessor(node);
     }
+    if(!bba.check()){
+        console.log("delete error");
+        return false;
+    }
+    return true;
+}
+function test2(n = 1000){
+    for(let i = 0; i < n; i ++) if(!test()) return false;
     return true;
 }
