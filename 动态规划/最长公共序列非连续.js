9460146d0c2ba835445c4d316c834f8863fceccb
ft-noone <1430621980@qqcom>
2021-04-22 17:44:09 +0800

    动态规划总结

--- 动态规划/最长公共序列非连续.js
+++ 动态规划/最长公共序列非连续.js
@@ -1,14 +1,9 @@
 function longestCommonSequence(x, y){
-    var m = x.length, n = y.length;
-    var c = new Array(m).forEach(()=>new Array(n));
-    for(let i = 1; i < m; i ++) c[i][0] = 0;
-    for(let i = 0; i < n; i ++) c[0][i] = 0;
-    for(let i = 1, j; i < m; i ++){
-        for(j = 1; j < n; j ++){
-            if(x[i] == y[j]) c[i][j] = c[i - 1][j - 1] + 1;
-            else if(c[i - 1][j] <= c[i][j - 1]) c[i][j] = c[i][j - 1];
-            else c[i][j] = c[i - 1][j];
-        }
+    var m = x.length, n = y.length, i, j, len = n < m ? n : m, subarr = new Array(len);
+    for(i = 0; i < len; i ++){
+        subarr[i] = new Array(len);
     }
+    
+    for(i = 0; i < )
     return c;
 }
